5618edaf980a11ca9f37272ad19a211be7a9d649
k1LoW <k1lowxb@gmail.com>
2012-08-25 10:47:28 +0900

    Initial commit

---
+++ RandomWithSeedPlugIn.h
@@ -0,0 +1,18 @@
+//
+//  RandomWithSeedPlugIn.h
+//  Random With Seed PlugIn for Quartz Composer
+//
+//  Created by k1LoW on 12/07/28.
+//  Copyright (c) 2012 101000LAB/101000code. All rights reserved.
+//
+
+#import <Quartz/Quartz.h>
+
+@interface RandomWithSeedPlugIn : QCPlugIn
+{
+}
+
+@property double inputNumber;
+@property double outputNumber;
+
+@end
---
+++ RandomWithSeedPlugIn.m
@@ -0,0 +1,150 @@
+//
+//  RandomWithSeedPlugIn.m
+//  Random With Seed PlugIn for Quartz Composer
+//
+//  Created by k1LoW on 12/07/28.
+//  Copyright (c) 2012 101000LAB/101000code. All rights reserved.
+//
+
+/* It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering */
+#import <OpenGL/CGLMacro.h>
+
+#import "RandomWithSeedPlugIn.h"
+
+#define	kQCPlugIn_Name				@"Random With Seed"
+#define	kQCPlugIn_Description		@"Untitled description"
+
+@implementation RandomWithSeedPlugIn
+
+@dynamic inputNumber, outputNumber;
+
+/*
+Here you need to declare the input / output properties as dynamic as Quartz Composer will handle their implementation
+@dynamic inputFoo, outputBar;
+*/
+
++ (NSDictionary*) attributes
+{
+	/*
+	Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
+	*/
+	
+	return [NSDictionary dictionaryWithObjectsAndKeys:kQCPlugIn_Name, QCPlugInAttributeNameKey, kQCPlugIn_Description, QCPlugInAttributeDescriptionKey, nil];
+}
+
++ (NSDictionary *)attributesForPropertyPortWithKey:(NSString *)key
+{
+    if([key isEqualToString:@"inputNumber"])
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                @"Seed", QCPortAttributeNameKey,
+                [NSNumber numberWithUnsignedInteger:64],  QCPortAttributeDefaultValueKey,
+                nil];
+    
+    if([key isEqualToString:@"outputNumber"])
+        return [NSDictionary dictionaryWithObjectsAndKeys:
+                @"Random", QCPortAttributeNameKey,
+                nil];
+	
+	return nil;
+}
+
++ (QCPlugInExecutionMode) executionMode
+{
+	/*
+	Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
+	*/
+	
+	return kQCPlugInExecutionModeProcessor;
+}
+
++ (QCPlugInTimeMode) timeMode
+{
+	/*
+	Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
+	*/
+	
+	return kQCPlugInTimeModeNone;
+}
+
+- (id) init
+{
+	if(self = [super init]) {
+		/*
+		Allocate any permanent resource required by the plug-in.
+		*/
+	}
+	
+	return self;
+}
+
+- (void) finalize
+{
+	/*
+	Release any non garbage collected resources created in -init.
+	*/
+	
+	[super finalize];
+}
+
+- (void) dealloc
+{
+	/*
+	Release any resources created in -init.
+	*/
+	
+	[super dealloc];
+}
+
+@end
+
+@implementation RandomWithSeedPlugIn (Execution)
+
+- (BOOL) startExecution:(id<QCPlugInContext>)context
+{
+	/*
+	Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
+	Return NO in case of fatal failure (this will prevent rendering of the composition to start).
+	*/
+	
+	return YES;
+}
+
+- (void) enableExecution:(id<QCPlugInContext>)context
+{
+	/*
+	Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
+	*/
+}
+
+- (BOOL) execute:(id<QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary*)arguments
+{
+	/*
+	Called by Quartz Composer whenever the plug-in instance needs to execute.
+	Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
+	Return NO in case of failure during the execution (this will prevent rendering of the current frame to complete).
+	
+	The OpenGL context for rendering can be accessed and defined for CGL macros using:
+	CGLContextObj cgl_ctx = [context CGLContextObj];
+	*/
+	
+	srand((unsigned)(CFAbsoluteTimeGetCurrent() * self.inputNumber));
+	self.outputNumber = rand() * rand() % 100;
+	
+	return YES;
+}
+
+- (void) disableExecution:(id<QCPlugInContext>)context
+{
+	/*
+	Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
+	*/
+}
+
+- (void) stopExecution:(id<QCPlugInContext>)context
+{
+	/*
+	Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
+	*/
+}
+
+@end
